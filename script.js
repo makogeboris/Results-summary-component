3a773407625cb66badcc4d1cd0f8fdc6ab2334b6
makoge boris <makogeboris@gmail.com>
2024-06-05 09:57:17 +0100

    modified

--- script.js
+++ script.js
@@ -7,37 +7,27 @@ fetch(url)
   .then((res) => res.json())
   .then((data) => {
     addData(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
   });
 
 function addData(data) {
   let html = "";
 
-  // Define a mapping for the categories to their corresponding classes
-  const categoryClassMap = {
-    reaction: "reaction",
-    memory: "memory",
-    verbal: "verbal",
-    visual: "visual",
-  };
-
   data.forEach((item) => {
-    // Ensure the category is in the categoryClassMap
-    if (categoryClassMap.hasOwnProperty(item.category.toLowerCase())) {
-      const itemClass = categoryClassMap[item.category.toLowerCase()];
+    const categoryClass = item.category.toLowerCase();
 
-      html += `<div class="summary-scores ${itemClass}">
+    html += `<div class="summary-scores ${categoryClass}">
       <div class="summary-icon-p">
         <img class="icon" src="${item.icon}" alt="" />
-        <p class="category ${itemClass}-p">${item.category}</p>
+        <p class="category ${categoryClass}-p">${item.category}</p>
       </div>
       <p class="score">
         ${item.score}
         <span class="total">/ 100</span>
       </p>
     </div>`;
-    } else {
-      console.warn(`Unknown category: ${item.category}`);
-    }
   });
 
   summary.innerHTML = html;
